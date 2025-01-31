fabee685f9ffd48d6e519f1a07975f6688c2b6bc
Raiyan-sharif <raiyan.sharif.1234@gmail.com>
2025-01-31 08:49:55 +0600

    AppLifeCycle init

---
+++ AppLifeCycleUIKit/AppLifeCycleUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppLifeCycleUIKit
+//
+//  Created by BJIT on 31/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
